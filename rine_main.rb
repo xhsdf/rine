e0c8940990ea8316e1206741d02e146157133bfc
xhsdf <xhsdf.misc@googlemail.com>
2015-03-01 15:53:11 +0100

    removed token

--- rine_main.rb
+++ rine_main.rb
@@ -5,7 +5,7 @@ require 'line_management'
 
 
 def main()
-	Management.new("username", "password", "DP3RQWRA1Va3mb6mClR4.BhyP8nXvxerRSWcRZ++Vva.OC/C8ILKfpOfk7NKcOV8bWIex5IZ0zWN4FPqsINwWFs=").run()
+	Management.new("username", "password", "token").run()
 end
 
 
